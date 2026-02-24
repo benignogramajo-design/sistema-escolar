841097ad4c8d3984c402bf1c1296a1abf71b0f2a
Benigno Gramajo <benignogramajo@gmail.com>
2026-02-24 01:19:08 -0300

    Forzar actualizacion de imagen en MesasDeExamen

--- src/pages.css/submenu/MesasDeExamen.jsx
+++ src/pages.css/submenu/MesasDeExamen.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../../components.css/NavBar";
 import "../../styles/pages.css";
-import fondo from "../../assets.css/fondos/Fondo PRECEPTORIA1.jpg";
+import fondo from "../../assets.css/fondos/Fondo PRECEPTORIA1.jpg"; // Corrección de extensión
 import { supabase } from "../../components.css/supabaseClient";
 import logo from "../../assets.css/logos/Logo.png";
 
